b47482ad06b0edc61888bd81aae2b1d88d64169f
Madfater <ericleeddd@gmail.com>
2024-02-20 23:53:17 +0800

    feat: 可以新增廣告了

--- models/advertisment.go
+++ models/advertisment.go
@@ -2,24 +2,19 @@ package models
 
 import (
 	"time"
+
 	"gorm.io/datatypes"
 )
 
 type Advertisement struct {
 	ID         uint      `gorm:"primaryKey"`
 	Title      string    `gorm:"not null"`
-	StartAt    time.Time `gorm:"not null"`
-	EndAt      time.Time `gorm:"not null"`
+	StartAt    time.Time `gorm:"not null;column:StartAt"`
+	EndAt      time.Time `gorm:"not null;column:EndAt"`
 	Conditions datatypes.JSON   `gorm:"not null"`
 }
 
 
 func (Advertisement) TableName() string {
 	return "advertisement"
-}
-
-
-func GetAdList()(adlist []Advertisement){
-	DB.Find(&adlist)
-	return
 }
--- routers/controlers/admin.go
+++ routers/controlers/admin.go
@@ -3,15 +3,53 @@ package controlers
 import (
 	"dcardAssignment/models"
 	
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type AdminControler struct{}
 
-func (AdminControler) CreateAdvertisement(c *gin.Context) {
+type Body struct {
+	Title      string    `json:"title"`
+	StartAt    time.Time `json:"startAt"`
+	EndAt      time.Time `json:"endAt"`
+	Conditions struct {
+		AgeStart int      `json:"ageStart"`
+		AgeEnd   int      `json:"ageEnd"`
+		Country  []string `json:"country"`
+		Platform []string `json:"platform"`
+	} `json:"conditions"`
+}
 
-	var data []models.Advertisement = models.GetAdList()
-	c.JSON(http.StatusOK, data)
+func (AdminControler) CreateAdvertisement(c *gin.Context) {
+	var body Body
+	if err := c.ShouldBindJSON(&body); err != nil {
+		// 處理錯誤
+		c.JSON(http.StatusBadRequest, gin.H{"Loadin Body failed": err.Error()})
+		return
+	}
+
+	json, err := json.Marshal(body.Conditions)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"JSON encoding failed:": err})
+		return
+	}
+
+	ad := models.Advertisement{
+		Title:      body.Title,
+		StartAt:    body.StartAt,
+		EndAt:      body.EndAt,
+		Conditions: json,
+	}
+
+	res := models.DB.Create(&ad)
+	if res.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Create failed:": err})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{"Message":"建立成功"})
 }
