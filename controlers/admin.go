96e047289ce3d24afe3a3d1e4b495d5c18728d19
Madfater <ericleeddd@gmail.com>
2024-02-27 02:09:26 +0800

    feat: 新增body驗證

--- controlers/admin.go
+++ controlers/admin.go
@@ -2,26 +2,30 @@ package controlers
 
 import (
 	"dcardAssignment/models"
+	"fmt"
 
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 type AdminControler struct{}
 
 type Body struct {
-	Title      string    `json:"title"`
-	StartAt    time.Time `json:"startAt"`
-	EndAt      time.Time `json:"endAt"`
-	Conditions struct {
-		AgeStart int      `json:"ageStart"`
-		AgeEnd   int      `json:"ageEnd"`
-		Country  []string `json:"country"`
-		Platform []string `json:"platform"`
-		Gender   *string  `json:"gender"`
-	} `json:"conditions"`
+	Title      string     `json:"title" validate:"required"`
+	StartAt    time.Time  `json:"startAt" validate:"required"`
+	EndAt      time.Time  `json:"endAt" validate:"required"`
+	Conditions Conditions `json:"conditions" validate:"required"`
+}
+
+type Conditions struct {
+	AgeStart int      `json:"ageStart" validate:"min=1,max=100"`
+	AgeEnd   int      `json:"ageEnd" validate:"min=1,max=100"`
+	Country  []string `json:"country"`
+	Platform []string `json:"platform"`
+	Gender   *string  `json:"gender" `
 }
 
 func (AdminControler) CreateAdvertisement(c *gin.Context) {
@@ -30,6 +34,43 @@ func (AdminControler) CreateAdvertisement(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Loadin Body failed": err.Error()})
+		return
+	}
+
+	if body.Conditions.AgeStart == 0 {
+		body.Conditions.AgeStart = 1
+	}
+	if body.Conditions.AgeEnd == 0 {
+		body.Conditions.AgeEnd = 100
+	}
+
+	//驗證query
+	validate := validator.New()
+
+	err := validate.Struct(body)
+	if err != nil {
+		var errMsg = "=== error msg ====\n"
+		errMsg += fmt.Sprintf("%s\n", err)
+
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			errMsg += fmt.Sprintf("%s\n", err)
+			return
+		}
+
+		errMsg += "=========== error field info ====================\n"
+		for _, err := range err.(validator.ValidationErrors) {
+			errMsg += fmt.Sprintf("Namespace: %s", err.Namespace())
+			errMsg += fmt.Sprintf("Fild: %s", err.Field())
+			errMsg += fmt.Sprintf("StructNamespace: %s", err.StructNamespace())
+			errMsg += fmt.Sprintf("StructField: %s", err.StructField())
+			errMsg += fmt.Sprintf("Tag: %s", err.Tag())
+			errMsg += fmt.Sprintf("ActualTag: %s", err.ActualTag())
+			errMsg += fmt.Sprintf("Kind: %s", err.Kind())
+			errMsg += fmt.Sprintf("Type: %s", err.Type())
+			errMsg += fmt.Sprintf("Value: %s", err.Value())
+			errMsg += fmt.Sprintf("Param: %s", err.Param())
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"Error Message": errMsg})
 		return
 	}
 
