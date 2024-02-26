7ba4983e69e373f43932c99fcb3b22f10a3a7180
Madfater <ericleeddd@gmail.com>
2024-02-27 00:05:50 +0800

    fix: 新增錯誤ad的測試

--- controlers/admin_test.go
+++ controlers/admin_test.go
@@ -60,3 +60,51 @@ func TestCreateAdvertisement(t *testing.T) {
 
 	assert.Equal(t, "建立成功", response["Message"])
 }
+
+func TestCreateVaildAdvertisement(t *testing.T) {
+
+	gender:= "G"
+	body := controlers.Body{
+		Title: "Sample Advertisement",
+		StartAt: time.Now(),
+		EndAt: time.Now().Add(24 * time.Hour), // 结束时间为当前时间后一天
+		Conditions: struct {
+			AgeStart int      `json:"ageStart"`
+			AgeEnd   int      `json:"ageEnd"`
+			Country  []string `json:"country"`
+			Platform []string `json:"platform"`
+			Gender   *string  `json:"gender"`
+		}{
+			AgeStart: 18,
+			AgeEnd:   50,
+			Country:  []string{"TW", "JP"},
+			Platform: []string{"android", "ios"},
+			Gender:   &gender, 
+		},
+	}
+	jsonBody, _ := json.Marshal(body)
+
+	r := gin.Default()
+	
+	r.POST("/createAdvertisement", controlers.AdminControler{}.CreateAdvertisement)
+
+	req, err := http.NewRequest("POST", "/createAdvertisement", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	fmt.Println(w.Body)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	var response map[string]interface{}
+	if err := json.Unmarshal(w.Body.Bytes(), &response); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, "建立成功", response["Create failed:"])
+}
