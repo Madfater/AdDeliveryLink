54c25c83cbe97f34684580147f981b7a8e62a93b
Madfater <ericleeddd@gmail.com>
2024-02-27 02:09:04 +0800

    feat: query改為optional

--- controlers/public.go
+++ controlers/public.go
@@ -18,13 +18,13 @@ type Item struct {
 	EndAt time.Time `json:"EndAt"`
 }
 
-type QueryParams struct {
+type PublicValidator struct {
 	Offset   *int   `validate:"required,min=0"`
 	Limit    *int   `validate:"required,min=1"`
-	Age      *int   `validate:"required,min=1,max=100"`
-	Gender   string `validate:"required,oneof=M F"`
-	Country  string `validate:"required"`
-	Platform string `validate:"required"`
+	Age      *int   `validate:"min=0,max=100"`
+	Gender   string `validate:"omitempty,oneof=M F"`
+	Country  string
+	Platform string
 }
 
 func (PublicControler) PublicAdvertisement(c *gin.Context) {
@@ -39,7 +39,7 @@ func (PublicControler) PublicAdvertisement(c *gin.Context) {
 	//驗證query
 	validate := validator.New()
 
-	queryParams := QueryParams{
+	queryParams := PublicValidator{
 		Offset:   &offset,
 		Limit:    &limit,
 		Age:      &age,
@@ -77,19 +77,37 @@ func (PublicControler) PublicAdvertisement(c *gin.Context) {
 
 	var selectResult []Item
 
-	models.DB.
+	query := models.DB.
 		Model(&models.Advertisement{}).
-		Select("advertisement.Title", "advertisement_country.country_code").
+		Select("DISTINCT advertisement.Title", "advertisement.EndAt").
 		Joins("left join advertisement_country on advertisement.ID = advertisement_country.advertisement_id").
-		Joins("left join advertisement_platform on advertisement.ID = advertisement_platform.advertisement_id").
-		Where(models.DB.Where("advertisement_country.country_code = ?", country).Or("advertisement_country.country_code is NULL")).
-		Where(models.DB.Where("advertisement_platform.platform_name = ?", platform).Or("advertisement_platform.platform_name is NULL")).
-		Where(models.DB.Where("advertisement.Gender = ?", gender).Or("advertisement.Gender is NULL")).
-		Where("advertisement.AgeStart <= ? AND advertisement.AgeEnd >= ?", age, age).
+		Joins("left join advertisement_platform on advertisement.ID = advertisement_platform.advertisement_id")
+
+	if platform != "" {
+		query.Where(models.DB.Where("advertisement_platform.platform_name = ?", platform).Or("advertisement_platform.platform_name is NULL"))
+	}
+
+	if country != "" {
+		query.Where(models.DB.Where("advertisement_country.country_code = ?", country).Or("advertisement_country.country_code is NULL"))
+	}
+
+	if gender != "" {
+		query.Where(models.DB.Where("advertisement.Gender = ?", gender).Or("advertisement.Gender is NULL"))
+	}
+
+	if age != 0 {
+		query.Where("advertisement.AgeStart <= ? AND advertisement.AgeEnd >= ?", age, age)
+	}
+
+	query.
 		Offset(offset).
 		Limit(limit).
 		Order("endAt asc").
 		Find(&selectResult)
+
+	if query.Error != nil {
+		c.JSON(http.StatusOK, gin.H{"Message": query.Error})
+	}
 
 	c.JSON(http.StatusOK, gin.H{"items": selectResult})
 }
--- controlers/public_test.go
+++ controlers/public_test.go
@@ -73,7 +73,7 @@ func TestMissingQuery(t *testing.T) {
 	
 	r.GET("/public/advertisement", controlers.PublicControler{}.PublicAdvertisement)
 
-	query := "offset=0&limit=10&age=25&gender=M&country=TW"
+	query := "offset=0&limit=10&age=25&gender=F&country=TW"
 
 	req, err := http.NewRequest("GET", "/public/advertisement?"+query, nil)
 	if err != nil {
@@ -85,12 +85,39 @@ func TestMissingQuery(t *testing.T) {
 
 	r.ServeHTTP(w, req)
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, http.StatusOK, w.Code)
 
 	var response map[string]interface{}
 	if err := json.Unmarshal(w.Body.Bytes(), &response); err != nil {
 		t.Fatal(err)
 	}
+	assert.NotNil(t,response["items"])
+}
 
-	assert.NotNil(t,response["Error Message"])
+func TestEmptyQuery(t *testing.T) {
+
+	r := gin.Default()
+	
+	r.GET("/public/advertisement", controlers.PublicControler{}.PublicAdvertisement)
+
+	query := "offset=0&limit=10"
+
+	req, err := http.NewRequest("GET", "/public/advertisement?"+query, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var response map[string]interface{}
+	if err := json.Unmarshal(w.Body.Bytes(), &response); err != nil {
+		t.Fatal(err)
+	}
+	
+	assert.NotNil(t,response["items"])
 }
