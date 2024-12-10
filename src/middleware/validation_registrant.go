61d760eb60a721c63ee61af4a6cc9473af633748
Madfater <linzhaoxuan0205@gmail.com>
2024-12-11 00:49:14 +0800

    refactor: 重構 validation_registrant.go 並移動位置

---
+++ src/middleware/validation_registrant.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"github.com/Madfater/AdDeliveryLink/enum"
+	"github.com/Madfater/AdDeliveryLink/log"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"reflect"
+)
+
+// RegisterCustomValidation 註冊自訂 Enum 驗證於 /go-playground/validator
+func RegisterCustomValidation() {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		err := v.RegisterValidation("enum", enumValidation)
+		log.HandleError(err, "validator register failed")
+	}
+}
+
+// enumValidation 校驗字段是否為有效的 Enum 值或 Enum Slice
+func enumValidation(fl validator.FieldLevel) bool {
+	fieldValue := fl.Field()
+
+	// 如果是 Slice，檢查每個元素
+	if fieldValue.Kind() == reflect.Slice {
+		for i := 0; i < fieldValue.Len(); i++ {
+			if !isEnumValid(fieldValue.Index(i).Interface()) {
+				return false
+			}
+		}
+		return true
+	}
+
+	// 單一值檢查
+	return isEnumValid(fieldValue.Interface())
+}
+
+// isEnumValid 檢查值是否實現了 EnumInterface 並有效
+func isEnumValid(value interface{}) bool {
+	if enum, ok := value.(enum.EnumInterface); ok {
+		return enum.IsValidEnum()
+	}
+	return false
+}
